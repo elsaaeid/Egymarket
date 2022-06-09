3fdba781c53b04910a0ddfe2dbdc31982395c2da
elsaaeid <92111076+elsaaeid@users.noreply.github.com>
2022-06-10 00:04:21 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,216 @@
+// chat content
+let menuToggle = document.querySelector(".toggle");
+let menu = document.querySelector(".menu");
+
+menuToggle.onclick = function(){
+  menu.classList.toggle('activing')
+  menuToggle.classList.toggle('activing')
+}
+const list = document.querySelectorAll("li");
+function activeLink() {
+  list.forEach((item) =>
+  item.classList.remove('activing'));
+  this.classList.add('activing')
+}
+ list.forEach((item) =>
+  item.addEventListener('click',activeLink));
+
+
+
+// Gmail content
+
+var sendBtn = document.getElementById("sendBtn");
+sendBtn.addEventListener('click',function(e){
+   e.preventDefault();
+   var name = document.getElementById("name").value;
+   var email = document.getElementById("email").value;
+   var subject = document.getElementById("subject").value;
+   var fullMessage = document.getElementById("fullMessage").value;
+   var body = 'name: ' +name + '</br> email: ' + email + '</br> subject' + subject + '</br> fullMessage' + fullMessage;
+
+   Email.send({
+    Host : "smtp.gmail.com",
+    Username : name,
+    Password : "jdpcxzjonvslmtfo",
+    To : 'www.saidsadaoy@gmail.com',
+    From : email,
+    Subject : subject,
+    Body : body
+}).then(
+  message => alert(fullMessage)
+);
+})
+
+
+
+// Global Variables
+
+const addCurrencyBtn = document.querySelector(".add-currency-btn");
+const addCurrencyList = document.querySelector(".add-currency-list");
+const currenciesList = document.querySelector(".currencies");
+
+
+let baseCurrency;
+let baseCurrencyAmount;
+
+
+// Event Listeners
+
+addCurrencyBtn.addEventListener("click", addCurrencyBtnClick);
+
+function addCurrencyBtnClick(event) {
+  addCurrencyBtn.classList.toggle("open");
+}
+
+addCurrencyList.addEventListener("click", addCurrencyListClick);
+
+function addCurrencyListClick(event) {
+  const clickedListItem = event.target.closest("li");
+  if(!clickedListItem.classList.contains("disabled")) {
+    const newCurrency = currencies.find(c => c.abbreviation===clickedListItem.getAttribute("data-currency"));
+    if(newCurrency) newCurrenciesListItem(newCurrency);
+  }
+}
+
+currenciesList.addEventListener("click", currenciesListClick);
+
+function currenciesListClick(event) {
+  if(event.target.classList.contains("close")) {
+    const parentNode = event.target.parentNode;
+    parentNode.remove();
+    addCurrencyList.querySelector(`[data-currency=${parentNode.id}]`).classList.remove("disabled");
+    if(parentNode.classList.contains("base-currency")) {
+      const newBaseCurrencyLI = currenciesList.querySelector(".currency");
+      if(newBaseCurrencyLI) {
+        setNewBaseCurrency(newBaseCurrencyLI);
+        baseCurrencyAmount = Number(newBaseCurrencyLI.querySelector(".input input").value);
+      }
+    }
+  }
+}
+
+function setNewBaseCurrency(newBaseCurrencyLI) {
+  newBaseCurrencyLI.classList.add("base-currency");
+  baseCurrency = newBaseCurrencyLI.id;
+  const baseCurrencyRate = currencies.find(currency => currency.abbreviation===baseCurrency).rate;
+  currenciesList.querySelectorAll(".currency").forEach(currencyLI => {
+    const currencyRate = currencies.find(currency => currency.abbreviation===currencyLI.id).rate;
+    const exchangeRate = currencyLI.id===baseCurrency ? 1 : (currencyRate/baseCurrencyRate).toFixed(4);
+    currencyLI.querySelector(".base-currency-rate").textContent = `1 ${baseCurrency} = ${exchangeRate} ${currencyLI.id}`;
+  });
+}
+
+currenciesList.addEventListener("input", currenciesListInputChange);
+
+function currenciesListInputChange(event) {
+  const isNewBaseCurrency = event.target.closest("li").id!==baseCurrency;
+  if(isNewBaseCurrency) {
+    currenciesList.querySelector(`#${baseCurrency}`).classList.remove("base-currency");
+    setNewBaseCurrency(event.target.closest("li"));
+  }
+  const newBaseCurrencyAmount = isNaN(event.target.value) ? 0 : Number(event.target.value);
+  if(baseCurrencyAmount!==newBaseCurrencyAmount || isNewBaseCurrency) {
+    baseCurrencyAmount = newBaseCurrencyAmount;
+    const baseCurrencyRate = currencies.find(currency => currency.abbreviation===baseCurrency).rate;
+    currenciesList.querySelectorAll(".currency").forEach(currencyLI => {
+      if(currencyLI.id!==baseCurrency) {
+        const currencyRate = currencies.find(currency => currency.abbreviation===currencyLI.id).rate;
+        const exchangeRate = currencyLI.id===baseCurrency ? 1 : (currencyRate/baseCurrencyRate).toFixed(4);
+        currencyLI.querySelector(".input input").value = exchangeRate*baseCurrencyAmount!==0 ? (exchangeRate*baseCurrencyAmount).toFixed(4) : "";
+      }
+    });
+  }
+}
+
+currenciesList.addEventListener("focusout", currenciesListFocusOut);
+
+function currenciesListFocusOut(event) {
+  const inputValue = event.target.value;
+  if(isNaN(inputValue) || Number(inputValue)===0) event.target.value="";
+  else event.target.value = Number(inputValue).toFixed(4);
+}
+
+currenciesList.addEventListener("keydown", currenciesListKeyDown);
+
+function currenciesListKeyDown(event) {
+  if(event.key==="Enter") event.target.blur();
+}
+
+// Auxiliary Functions
+
+function populateAddCyrrencyList() {
+  for(let i=0; i<currencies.length; i++) {
+    addCurrencyList.insertAdjacentHTML(
+      "beforeend", 
+      `<li data-currency=${currencies[i].abbreviation}>
+        <img src=${currencies[i].flagURL} class="flag"><span>${currencies[i].abbreviation} - ${currencies[i].name}</span>
+      </li>`
+    );
+  }
+}
+
+function populateCurrenciesList() {
+  for(let i=0; i<initiallyDisplayedCurrencies.length; i++) {
+    const currency = currencies.find(c => c.abbreviation===initiallyDisplayedCurrencies[i]);
+    if(currency) newCurrenciesListItem(currency);
+  }
+}
+
+function newCurrenciesListItem(currency) {
+  if(currenciesList.childElementCount===0) {
+    baseCurrency = currency.abbreviation;
+    baseCurrencyAmount = 0;
+  }
+  addCurrencyList.querySelector(`[data-currency=${currency.abbreviation}]`).classList.add("disabled");
+  const baseCurrencyRate = currencies.find(c => c.abbreviation===baseCurrency).rate;
+  const exchangeRate = currency.abbreviation===baseCurrency ? 1 : (currency.rate/baseCurrencyRate).toFixed(4);
+  const inputValue = baseCurrencyAmount ? (baseCurrencyAmount*exchangeRate).toFixed(4) : "";
+
+  currenciesList.insertAdjacentHTML(
+    "beforeend",
+    `<li class="currency ${currency.abbreviation===baseCurrency ? "base-currency" : ""}" id=${currency.abbreviation}>
+      <img src=${currency.flagURL} class="flag">
+      <div class="info">
+        <p class="input"><span class="currency-symbol">${currency.symbol}</span><input placeholder="0.0000" value=${inputValue}></p>
+        <p class="currency-name">${currency.abbreviation} - ${currency.name}</p>
+        <p class="base-currency-rate">1 ${baseCurrency} = ${exchangeRate} ${currency.abbreviation}</p>
+      </div>
+      <span class="close">&times;</span>
+    </li>`
+  );
+}
+
+fetch(dataURL)
+  .then(res => res.json())
+  .then(data => {
+    document.querySelector(".date").textContent = data.date;
+    data.rates["EUR"] = 1;
+    currencies = currencies.filter(currency => data.rates[currency.abbreviation]);
+    currencies.forEach(currency => currency.rate = data.rates[currency.abbreviation]);
+    populateAddCyrrencyList();
+    populateCurrenciesList();
+  })
+  .catch(err => console.log(err));
+
+
+
+
+
+
+// Search
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
